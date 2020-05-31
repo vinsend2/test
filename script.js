61cb0704546fdb2999e742b40793b6f04ec4c11b
Stas <vins_and@mail.ru>
2020-06-01 02:04:38 +0500

    1 задание

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 'use strict';
 
-var LeftBorderRight = 1;
-console.log(LeftBorderRight);
+let money = prompt("Ваш бюджет на месяц?"); 
+let time = prompt("Введите дату в формате YYYY-MM-DD"); 
 
 
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+
+};
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ""),
+a2 = prompt("Во сколько обойдется", ""),
+a3 = prompt("Введите обязательную статью расходов в этом месяце", ""),
+a4 = prompt("Во сколько обойдется", ""); 
+
+appData.expenses[a1] = a2;
+appData.expenses[a3] = a4;
+
+alert(appData.budget / 30);
