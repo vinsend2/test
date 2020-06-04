50ff193a17a035028fe4be8713cd8648b4a88a9b
Stas <vins_and@mail.ru>
2020-06-04 15:41:11 +0500

    6 задание

--- задание 6/js/script.js
+++ задание 6/js/script.js
@@ -3,25 +3,19 @@
 let menu = document.getElementsByClassName("menu")[0],
     menuItem = document.getElementsByClassName("menu-item"),
     adv = document.getElementsByClassName("adv")[0],
-    titleOld = document.getElementById(`title`),
+    title = document.getElementById(`title`),
     promptNativ = document.getElementById(`prompt`),
-    titleNew = document.createElement(`div`),
     column = document.querySelectorAll(`.column`),
     newli = document.createElement(`li`);
-        
 
-menu.insertBefore(menuItem[2], menuItem[1]);   
-titleNew.className = `title`;
-titleNew.id = `title`;
+
+menu.insertBefore(menuItem[2], menuItem[1]);
 newli.className = `menu-item`;
 newli.textContent = `Пятый пункт`;
 menu.appendChild(newli);
 document.body.style.backgroundImage = "url('/задание 6/img/apple_true.jpg')";
-console.log(adv);
-titleNew.textContent = `Мы продаем только подлинную технику Apple`;
-column[1].replaceChild(titleNew, titleOld);
+title.textContent = `Мы продаем только подлинную технику Apple`;
 column[1].removeChild(adv);
 
 let promptNew = prompt(`Ваше отношение к технике Apple?`, ``);
 promptNativ.textContent = promptNew;
-
